584e163ef4497d1a2be64806f7aa65c64866e898
maevet78 <190439mt@saintviator.com>
2018-08-08 21:17:03 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,3 +1,5 @@
+
+
 def least_coins(cents)
 coin_value = {
   "Quarter" => 0,
@@ -5,7 +7,7 @@ coin_value = {
   "Nickle" => 0,
   "Penny" => 0
 }
-  loop do 
+until cents == 0
     if cents >= 25
       cents - 25
       coin_value["Quarter"] += 1
@@ -15,18 +17,11 @@ coin_value = {
     elsif cents >= 5
       cents - 5
       coin_value["Nickle"] += 1
-    elsif cents >= 1
+    else cents >= 1
       cents - 1
       coin_value["Penny"] += 1
-    else
-      break
     end   
-  end
  puts coin_value
-end 
-
-least_coins(80)
-
-# In the `least_coins.rb` file, write a method called `least_coins`.
-
-# Given a number of cents as an argument, the method should return a hash with the least number of coins that it would take to make that amount. For example, running `least_coins(29)` should return `{:quarters => 1, :dimes => 0, :nickels => 0, :pennies => 4}`. 
+end
+end
+least_coins(50)
